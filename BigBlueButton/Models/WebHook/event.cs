f8c5e73d101be29a8c420c3c91a5f975bb8e1242
Mohammad Hadi Eskandari <hadi.eskandari@ymail.com>
2018-06-30 22:13:20 +0430

    some changes

--- BigBlueButton/Models/WebHook/event.cs
+++ BigBlueButton/Models/WebHook/event.cs
@@ -1,55 +1,64 @@
-﻿namespace BigBlueButton.Models.WebHook
+﻿using System.Collections.Generic;
+
+namespace BigBlueButton.Models.WebHook
 {
     //{[event, {"payload":{"meeting_id":"e6c3dd630428fd54834172b8fd2735fed9416da4-1527010228485","user":{"role":"MODERATOR","presenter":true,"locked":false,"extern_userid":"ortnjtvmvjg5","phone_user":false,"webcam_stream":[],"emoji_status":"none","voiceUser":{"talking":false,"callername":"teacher@gmail.com","locked":false,"callernum":"teacher@gmail.com","muted":false,"web_userid":"ortnjtvmvjg5_1","joined":false,"userid":"ortnjtvmvjg5_1"},"name":"teacher@gmail.com","listenOnly":false,"avatarURL":"http://192.168.149.132/client/avatar.png","userid":"ortnjtvmvjg5_1","has_stream":false}},"header":{"name":"user_left_message","version":"0.0.1","current_time":1527010593930,"timestamp":18738556}}]}
     //"name":"teacher@gmail.com","listenOnly":false,"avatarURL":"http://192.168.149.132/client/avatar.png","userid":"ortnjtvmvjg5_1","has_stream":false}
     public class Event
     {
-        public Header header { set; get; }
-        public Payload payload { set; get; }
+        public Payload payload { get; set; }
+        public Header header { get; set; }
     }
 
     public class Header
     {
-        public string timestamp { set; get; }
-        public string name { set; get; }
-        public string current_time { set; get; }
-        public string version { set; get; }
+        public string name { get; set; }
+        public string version { get; set; }
+        public long current_time { get; set; }
+        public int timestamp { get; set; }
     }
 
     public class Payload
     {
-        public string meeting_id { set; get; }
+        public int duration { get; set; }
+        public string external_meeting_id { get; set; }
+        public long create_time { get; set; }
+        public string meeting_id { get; set; }
+        public bool is_breakout { get; set; }
+        public string name { get; set; }
+        public string moderator_pass { get; set; }
+        public bool recorded { get; set; }
+        public string voice_conf { get; set; }
+        public string viewer_pass { get; set; }
+        public string create_date { get; set; }
         public User user { set; get; }
     }
 
     public class User
     {
-        public string role { set; get; }
-        public bool presenter { set; get; }
-        public bool locked { set; get; }
-        public bool phone_user { set; get; }
-        public string extern_userid { set; get; }
-        public string emoji_status { set; get; }
-        public string[] webcam_stream { set; get; } //"webcam_stream":[]
-        public VoiceUser voiceuser { set; get; }
-        public string name { set; get; }
-        public bool listenOnly { set; get; }
-        public string avatarURL { set; get; }
-        public string userid { set; get; }
-        public bool has_stream { set; get; }
-
+        public string role { get; set; }
+        public bool presenter { get; set; }
+        public bool locked { get; set; }
+        public string extern_userid { get; set; }
+        public bool phone_user { get; set; }
+        public List<object> webcam_stream { get; set; }
+        public string emoji_status { get; set; }
+        public VoiceUser voiceUser { get; set; }
+        public string name { get; set; }
+        public bool listenOnly { get; set; }
+        public string avatarURL { get; set; }
+        public string userid { get; set; }
+        public bool has_stream { get; set; }
     }
-
     public class VoiceUser
     {
-        public bool talking { set; get; }
-        public string callername { set; get; }
-        public bool locked { set; get; }
-        public string callernum { set; get; }
-        public bool muted { set; get; }
-        public string web_userid { set; get; }
-        public bool joined { set; get; }
-        public string userid { set; get; }
-
+        public bool talking { get; set; }
+        public string callername { get; set; }
+        public bool locked { get; set; }
+        public string callernum { get; set; }
+        public bool muted { get; set; }
+        public string web_userid { get; set; }
+        public bool joined { get; set; }
+        public string userid { get; set; }
     }
 }
--- OnlineCourse/Areas/Api/Controllers/BigBlueButtonHooksController.cs
+++ OnlineCourse/Areas/Api/Controllers/BigBlueButtonHooksController.cs
@@ -1,26 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.IO;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web;
 using AutoMapper;
-using BigBlueButton;
 using BigBlueButton.Models.WebHook;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Server.Kestrel.Internal.System.Text.Encodings.Web.Utf8;
 using Newtonsoft.Json;
 using OnlineCourse.Core;
 using OnlineCourse.Core.Services;
 using OnlineCourse.Entity;
 using OnlineCourse.Panel.Utils.Extentions;
-using OnlineCourse.Panel.Utils.ViewModels;
 
 namespace OnlineCourse.Panel.Areas.Api.Controllers
 {
@@ -38,84 +27,102 @@ namespace OnlineCourse.Panel.Areas.Api.Controllers
 
                 var req = _httpContextAccessor.HttpContext.Request.Form;
                 var eventModel = JsonConvert.DeserializeObject<Event>(req["event"]);
-                var settings = new JsonSerializerSettings()
-                {
-                    ContractResolver = new OrderedContractResolver(),
-                    NullValueHandling = NullValueHandling.Ignore,
+                //var settings = new JsonSerializerSettings()
+                //{
+                //    ContractResolver = new OrderedContractResolver(),
+                //    NullValueHandling = NullValueHandling.Ignore,
                     
-                };
-                var eventt = "event=" + JsonConvert.SerializeObject(eventModel, Formatting.None, settings) + "&timestamp=" + req["timestamp"];
-                eventt = WebUtility.UrlEncode(eventt);
+                    
+                //};
+                //var eventt = "event=" + JsonConvert.SerializeObject(eventModel, Formatting.None, settings) + "&timestamp=" + req["timestamp"];
+                //eventt = WebUtility.UrlEncode(eventt);
                 //var eventt = "event=" + req["event"]+ "&timestamp=" + req["timestamp"];
-                var request = _httpContextAccessor.HttpContext.Request;
-                var uriBuilder = new UriBuilder
-                {
-                    Scheme = request.Scheme,
-                    Host = request.Host.Host,
-                    Path = "api/BigBlueButtonHooks/index",
-                    Query = "meetingid=" + meetingid
-                };
-                if (request.Host.Port != null)
-                {
-                    uriBuilder.Port = request.Host.Port.Value;
-                }
-                var bbb = new BBB(_config.BbbGetServerIpAddress(), _config.BbbGetServerId());
-                if (bbb.WebHookCheckSumIsOk(eventt, uriBuilder.ToString(), checksum))
-                {
-                    var a = "test";
-                }
+                //var request = _httpContextAccessor.HttpContext.Request;
+                //var uriBuilder = new UriBuilder
+                //{
+                //    Scheme = request.Scheme,
+                //    Host = request.Host.Host,
+                //    Path = "api/BigBlueButtonHooks/index",
+                //    Query = "meetingid=" + meetingid
+                //};
+                //if (request.Host.Port != null)
+                //{
+                //    uriBuilder.Port = request.Host.Port.Value;
+                //}
+
+                //todo checksum of webhook callback must be create . 
+                //var bbb = new BBB(_config.BbbGetServerIpAddress(), _config.BbbGetServerId());
+                //if (bbb.WebHookCheckSumIsOk(eventt, uriBuilder.ToString(), checksum))
+                //{
+                //    var a = "test";
+                //}
                 var meeting = _context.ClassRooms.SingleOrDefault(c => c.Id == meetingid.Value);
                 if (meeting == null)
                     return Json("there is not any meeting.");
-                switch (eventModel.header.name)
+                if (eventModel.header.name == WebHookEvents.meeting_created_message)
                 {
-                    case WebHookEvents.meeting_created_message:
-                        meeting.Status = ClassStatus.OnGoing;
-                        _context.SaveChanges();
-                        break;
-                    case WebHookEvents.meeting_destroyed_event:
+                    meeting.Status = ClassStatus.OnGoing;
+                    _context.SaveChanges();
+                }
+                else if (eventModel.header.name == WebHookEvents.meeting_destroyed_event)
+                {
+                    meeting.Status = ClassStatus.Complete;
+                    _context.SaveChanges();
+                }
+                else if (eventModel.header.name == WebHookEvents.user_joined_message)
+                {
+                    meeting.Status = ClassStatus.OnGoing;
+                    _context.SaveChanges();
+                }
+                else if (eventModel.header.name == WebHookEvents.user_left_message)
+                {
+                    if (eventModel.payload.user.presenter)
+                    {
                         meeting.Status = ClassStatus.Complete;
                         _context.SaveChanges();
-                        break;
-                    case WebHookEvents.user_joined_message:
-                        meeting.Status = ClassStatus.OnGoing;
-                        _context.SaveChanges();
-                        break;
-                    case WebHookEvents.user_left_message:
-                        if (eventModel.payload.user.presenter)
-                        {
-                            meeting.Status = ClassStatus.Complete;
-                            _context.SaveChanges();
-                        }
-                        break;
-                    case WebHookEvents.archive_started:
-                        break;
-                    case WebHookEvents.archive_ended:
-                        break;
-                    case WebHookEvents.process_ended:
-                        break;
-                    case WebHookEvents.process_started:
-                        break;
-                    case WebHookEvents.post_archive_started:
-                        break;
-                    case WebHookEvents.post_publish_started:
-                        break;
-                    case WebHookEvents.post_publish_ended:
-                        break;
-                    case WebHookEvents.post_archive_ended:
-                        break;
-                    case WebHookEvents.publish_started:
-                        break;
-                    case WebHookEvents.publish_ended:
-                        break;
-                    case WebHookEvents.post_process_ended:
-                        break;
-                    case WebHookEvents.post_process_started:
-                        break;
-                    case WebHookEvents.sanity_ended:
-                        break;
-                    case WebHookEvents.sanity_started:
-                        break;
+                    }
+                }
+                else if (eventModel.header.name == WebHookEvents.archive_started)
+                {
+                }
+                else if (eventModel.header.name == WebHookEvents.archive_ended)
+                {
+                }
+                else if (eventModel.header.name == WebHookEvents.process_ended)
+                {
+                }
+                else if (eventModel.header.name == WebHookEvents.process_started)
+                {
+                }
+                else if (eventModel.header.name == WebHookEvents.post_archive_started)
+                {
+                }
+                else if (eventModel.header.name == WebHookEvents.post_publish_started)
+                {
+                }
+                else if (eventModel.header.name == WebHookEvents.post_publish_ended)
+                {
+                }
+                else if (eventModel.header.name == WebHookEvents.post_archive_ended)
+                {
+                }
+                else if (eventModel.header.name == WebHookEvents.publish_started)
+                {
+                }
+                else if (eventModel.header.name == WebHookEvents.publish_ended)
+                {
+                }
+                else if (eventModel.header.name == WebHookEvents.post_process_ended)
+                {
+                }
+                else if (eventModel.header.name == WebHookEvents.post_process_started)
+                {
+                }
+                else if (eventModel.header.name == WebHookEvents.sanity_ended)
+                {
+                }
+                else if (eventModel.header.name == WebHookEvents.sanity_started)
+                {
                 }
                 return Json("ok");
             }
