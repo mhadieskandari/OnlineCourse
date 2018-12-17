fa39c6415c7ec8f266fbbb6fc814f36895163e57
Mohammad Hadi Eskandari <hadi.eskandari@ymail.com>
2018-12-18 00:32:28 +0330

    roleback this commit

--- OnlineCourse/Program.cs
+++ OnlineCourse/Program.cs
@@ -13,6 +13,7 @@ namespace OnlineCourse
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseUrls("localhost://")
                 .Build();
     }
 }
